ca39e170b6056d45c16a7a6d9e81cf9ec91cf684
stendro <33956801+stendro@users.noreply.github.com>
2022-06-16 19:53:21 +0200

    Revert "qos: Don't disable interrupts while holding pm_qos_lock"
    This reverts commit 5ab6428383040fe7b84da05bdd924ceeb82f51c6.
    
    Update ir from mk2000

--- build.sh
+++ build.sh
@@ -95,9 +95,9 @@ THREADS=$(grep -c "processor" /proc/cpuinfo)
 # directory containing cross-compiler
 # a newer toolchain (gcc8+) is recommended due to changes made
 # to the kernel.
-GCC_COMP=$HOME/Kernel/toolchain/gcc-arm-10.3-2021.07-x86_64-aarch64-none-elf/bin/aarch64-none-elf-
+GCC_COMP=$HOME/toolchains/gcc-10.3-aarch64-main/bin/aarch64-none-elf-
 # directory containing 32bit cross-compiler for CONFIG_COMPAT_VDSO
-GCC_COMP_32=$HOME/Kernel/toolchain/gcc-arm-10.3-2021.07-x86_64-arm-none-eabi/bin/arm-none-eabi-
+GCC_COMP_32=$HOME/toolchains/gcc-10.3-arm-main/bin/arm-eabi-
 
 # -------------------------------- END -----------------------------------
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
